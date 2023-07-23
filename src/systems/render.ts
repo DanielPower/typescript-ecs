01025e34460e633906d7c5adf8fbf74f79e2eaa0
Daniel Power <me@danielpower.ca>
2023-07-23 14:50:39 -0230

    Use Two.js for rendering

--- src/systems/render.ts
+++ src/systems/render.ts
@@ -1,27 +1,30 @@
+import Two from "two.js";
 import { Component } from "../components.ts";
 import { createSystem } from "../ecs.ts";
 
 const canvas = document.getElementById("game") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
-if (!ctx) {
-  throw new Error("Could not get canvas context");
-}
+const two = new Two({
+  type: Two.Types.webgl,
+  domElement: canvas,
+});
 
 export const Render = createSystem({
   pools: {
     main: new Set([Component.Position, Component.Size]),
   },
   render(ecs, pools) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    two.clear();
     for (const entity of pools.main) {
       const components = ecs.getComponents(entity);
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const { x, y } = components[Component.Position]!;
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const { x: width, y: height } = components[Component.Size]!;
-      ctx.strokeStyle = "red";
-      ctx.strokeRect(x, y, width, height);
+      const circle = two.makeCircle(x, y, width);
+      circle.stroke = "orangered";
+      circle.fill = "#FF8000";
+      circle.linewidth = 5;
     }
+    two.update();
   },
 });
